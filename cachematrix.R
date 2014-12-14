337cf215bf3f5385c17e8ece43defe80b7f52fc9
lxm <liu_x_m@hotmail.com>
2014-12-14 14:05:49 +0800

    添加注释
    添加了函数注释

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 可以缓存逆矩阵的两个函数功能
 
-## Write a short comment describing this function
 
+## makeCacheMatrix函数用于创建可缓存逆矩阵的特殊“矩阵”对象。
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -12,6 +11,7 @@ makeCacheMatrix <- function(x = matrix()) {
   get <- function() x
   setinver <- function(inverse) i <<- inverse
   getinver <- function() i
+    ##创建List列表，分别储存四个函数set,get,setinver,getinver。
   list(set = set, get = get,
        setinver = setinver,
        getinver = getinver)
@@ -19,15 +19,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## 此函数用于计算makeCacheMatrix返回的特殊“矩阵”的逆矩阵
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## 返回传入矩阵的逆矩阵
   i <- x$getinver()
+        ##如果已经计算逆矩阵，检索缓存中的逆矩阵应存在。
   if(!is.null(i)) {
-    message("getting cached data")
+    message("getting cached data")   ##输出提示并返回缓存中得逆矩阵
     return(i)
   }
+        ##否则重新计算逆矩阵并返回。
   data <- x$get()
   i <- solve(data)
   x$setinver(i)
